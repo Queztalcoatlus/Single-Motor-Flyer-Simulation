2e2e4f8de419fd8a06408bd9f3a6a02a7fb559c4
Dong Wei <dongweihorizon@gmail.com>
2018-08-05 18:16:14 -0700

    flyer module is now fixed

--- dynamics_test.m
+++ dynamics_test.m
@@ -1,7 +1,13 @@
-function [x,x_dot,R,theta,omega] = dynamics_test(f,x_old,x_dot_old,theta_old,omega_old)
+f_last
+function [f_last,x,x_dot,R,theta,omega,counter] = fcn(f_old,f,x_old,x_dot_old,theta_old,omega_old,counter_old)
 % the block is based on the dynamics of a quadcopter on
 % http://andrew.gibiansky.com/blog/physics/quadcopter-dynamics/
-    
+    counter=counter_old+1;
+    if counter==15
+        counter=15;
+    end
+%Assume a friction coeffiicent
+    kd=0.25;
     %initial states
     %row, pitch and yaw
     theta = zeros(3,1);
@@ -23,23 +29,28 @@ function [x,x_dot,R,theta,omega] = dynamics_test(f,x_old,x_dot_old,theta_old,ome
     % Compute thrust given current inputs and thrust coefficient.
     %f = [0; 0; 6.41*10^(-6) * input*input];
     prop_speed_sq=f/(6.41*10^(-6));
-    prop_speed=[0;0;sqrt(prop_speed_sq)];
+    prop_speed=[0;0;sqrt(abs(prop_speed_sq))];
+    prop_speed_sq_old=f_old/(6.41*10^(-6));
+    prop_speed_old=[0;0;sqrt(abs(prop_speed_sq_old))];
     % Compute reaction torque, given current inputs, length, drag coefficient, and thrust coefficient.
     T_p = -prop_speed_sq* 1.1 * 10e-7;
-    f_vector=[0; 0; f];
+    f_vector=[0; 0; -f];
     % Compute forces, torques, and accelerations.
-    omega = thetadot2omega(thetadot, theta);
+    %omega = thetadot2omega(thetadot, theta);
+    %This seems to be wrong
     %acceleration in earth frame
-    %R = rotation(theta);%rotation matrix from body frame to earth frame
-    R=zeros(3,3);
+    R = rotation(theta_old);%rotation matrix from body frame to earth frame
+    %R=zeros(3,3);
     T = R * f_vector;
-    a = g + 1 / m .* T;
+    a = g + 1 / m .* T-kd*x_dot_old.^2;
     %omegadot
-    t_B_d=-sqrt(sum((omega).^2))*K_B_d*omega;
+    t_B_d=-sqrt(sum((prop_speed).^2))*K_B_d*prop_speed;
     tau=cross(d',f_vector)+T_p;%total torque
-
-    omegadot = I_B_B\(tau + t_B_d - cross(omega, I_B_B * omega+I_B_P*(R*prop_speed)));
-
+    prop_ang_acc=(prop_speed-prop_speed_old)/dt;
+    omegadot = I_B_B\ (tau + t_B_d - cross(omega_old, I_B_B * omega_old+I_B_P*(R*prop_speed))-I_B_P*(R*prop_ang_acc));
+    if max(omegadot)>10^20
+        omegadot=[0;0;0];
+    end
     % Advance system state
     omega = omega_old + dt .* omegadot;
     %omega=[0;0;0];
@@ -48,7 +59,9 @@ function [x,x_dot,R,theta,omega] = dynamics_test(f,x_old,x_dot_old,theta_old,ome
     x_dot = x_dot_old + a * dt;
     %x_dot=[0;0;0];
     x = x_old + dt * x_dot;
+    f_last=f;
     %x=[0;0;0];
     %SetOutputPortDimensions(s, 2, [3 1]);
 end
+
 
