b0f38cbc456239e3742991902a6f98abf8faa111
Wei Dong <dongweihorizon@gmail.com>
2019-02-21 02:04:48 -0800

    remove old files

--- dynamics_caller.m
+++
@@ -1,4 +0,0 @@
-%The caller calls the function of the flyer block
-clear;clc;
-%[x,x_dot,R,theta,omega] = fcn(f,x_old,x_dot_old,theta_old,omega_old)
-[x,x_dot,R,theta,omega] = dynamics_test(1,[1;1;1],[1;1;1],[1;1;1],[1;1;1]);
